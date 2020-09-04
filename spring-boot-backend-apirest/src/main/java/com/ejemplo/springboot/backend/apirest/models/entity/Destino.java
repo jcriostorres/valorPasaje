496139ccc1d6afc18e621774b5360e8f338051b5
Johana catalina rios torres <jcriostorres@gmail.com>
2020-09-04 09:29:38 -0700

    adicion de varibles y una constante, restructura del codigo.

--- spring-boot-backend-apirest/src/main/java/com/ejemplo/springboot/backend/apirest/models/entity/Destino.java
+++ spring-boot-backend-apirest/src/main/java/com/ejemplo/springboot/backend/apirest/models/entity/Destino.java
@@ -8,29 +8,32 @@ public class Destino {
 	private long id;
 	private int dias;
 	private int km;
-	private String origen;
-	private String destination;
-	private double DESCUENTO;
-	private double total;
+	final double DESCUENTO = 0.3;
+	private double PRECIO = 35000;
+	private double valorTotalSinDescuento;
+	private double valorTotalConDescuento;
+	private double valorDescuento;
 
-	// metodo
+	// pide datos de entrada
 
 	public void calcularPasaje() {
 		JOptionPane.showMessageDialog(null, "Calcular pasaje");
 		int dias = Integer.parseInt(JOptionPane.showInputDialog("ingrese cuantos dias sera su viaje"));
 		int km = Integer.parseInt(JOptionPane.showInputDialog("Ingrese los kilometros de recorrido de el viaje"));
 
+		//calculos
+		double valorTotalSinDescuento = km * PRECIO;
+		valorDescuento = valorTotalSinDescuento * DESCUENTO;
 		if (dias > 7 && km > 1000) {
-			DESCUENTO = 0.30;
+			
+			valorTotalConDescuento = valorTotalSinDescuento - valorDescuento;
+			JOptionPane.showMessageDialog(null, "el total con descuento a pagar de su viaje es: " + valorTotalConDescuento);
 		}
 
 		else {
-			DESCUENTO = 1;
+			valorTotalSinDescuento;
 		}
-		km = km * 35000;
-		DESCUENTO = km * DESCUENTO;
-		total = dias + km - DESCUENTO;
-		JOptionPane.showMessageDialog(null, "el total a pagar de su viaje es: " + total);
+		
+		JOptionPane.showMessageDialog(null, "el total sin descuento a pagar de su viaje es: " + valorTotalSinDescuento);
 	}
-
 }
